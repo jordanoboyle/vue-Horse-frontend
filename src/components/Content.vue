b5212161541cc27246a568fcd5f25c0d2f3fa1e7
jordanoboyle <jmcmcode@gmail.com>
2024-06-25 14:11:38 -0600

    basic data render on content page

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,41 @@
-<script></script>
+<script>
+import axios from "axios"
+import HorsesIndex from "./HorsesIndex.vue"
+
+
+export default {
+  components: {
+    HorsesIndex,
+  },
+
+  methods: {
+    getStuff: function() {
+      console.log("getting some Horses!");
+      axios.get("http://localhost:3000/horses.json").then(response => {
+        console.log(response.data);
+        this.horses = response.data; 
+      })
+    }
+  },
+  data: function () {
+    return {
+      horses: []
+    }
+  },
+}
+</script>
 
 <template>
   <main>
     <h1>Welcome to Vue!</h1>
+    <HorsesIndex />
+    {{ horses }}
+    <button v-on:click="getStuff" id="BRB">BIG RED BUTTON</button>
   </main>
 </template>
 
-<style></style>
+<style>
+#BRB {
+  color: red;
+}
+</style>
---
+++ src/components/HorsesIndex.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+  <div>
+    <h1>From the Horses Index (haha)</h1>
+  </div>
+</template>
+
+<style></style>
